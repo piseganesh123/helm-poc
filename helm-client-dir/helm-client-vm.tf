93e18d1cb7ebaa288d4750531364e21a991a16ef
Ganesh Pise <piseganesh123@gmail.com>
2021-10-23 13:35:42 +0530

    preemptible

--- helm-client-dir/helm-client-vm.tf
+++ helm-client-dir/helm-client-vm.tf
@@ -9,68 +9,6 @@ provider "google" {
  region      = var.region
 }
 
-/*module "network_firewall-rules" {
-  source  = "terraform-google-modules/network/google//modules/firewall-rules"
-  version = "3.4.0"
-  # insert the 2 required variables here
-}
-
-
-// Terraform plugin for creating random ids
-resource "random_id" "instance_id" {
- byte_length = 8
-}
-
-module "firewall_rules" {
-  source       = "terraform-google-modules/network/google//modules/firewall-rules"
-  project_id   = var.project_id
-//  network_name = google_compute_network.default.name
-  network_name = "default"
-  rules = [{
-    name                    = "allow-grafana-ingress-3000"
-    description             = "accepts grafana traffic"
-    direction               = "INGRESS"
-    priority                = null
-    ranges                  = ["0.0.0.0/0"]
-    source_tags             = null
-    source_service_accounts = null
-    target_tags             = ["allow-grafana-ingress-3000"]
-    target_service_accounts = null
-    allow = [{
-      protocol = "tcp"
-      ports    = ["3000"]
-    }]
-    deny = []
-    log_config = {
-      metadata = "EXCLUDE_ALL_METADATA"
-    }
-  },
-    {
-    name                    = "allow-prometheus-ingress-9090"
-    description             = "accepts grafana traffic"
-    direction               = "INGRESS"
-    priority                = null
-    ranges                  = ["0.0.0.0/0"]
-    source_tags             = null
-    source_service_accounts = null
-    target_tags             = ["allow-prometheus-ingress-9090"]
-    target_service_accounts = null
-    allow = [{
-      protocol = "tcp"
-      ports    = ["9090"]
-    }]
-    deny = []
-    log_config = {
-      metadata = "EXCLUDE_ALL_METADATA"
-    }
-  }]
-}
-
-
-resource "google_compute_network" "default" {
-  name = "demo-network"
-}*/
-
 // A single Compute Engine instance
 resource "google_compute_instance" "gcp-instance" {
  // name         = "prografana-poc-vm-${random_id.instance_id.hex}"
@@ -90,15 +28,19 @@ resource "google_compute_instance" "gcp-instance" {
 // metadata_startup_script = "sudo apt-get update; sudo apt-get install -yq build-essential python-pip rsync"
   metadata_startup_script = file("helm-config.sh")
 
- network_interface {
-   network = "default"
-   access_config {
-   }
- }
+  network_interface {
+    network = "default"
+    access_config {
+    }
+  }
    metadata = {
    ssh-keys = "$var.os-user:${file("~/.ssh/id_rsa.pub")}"
    enable-oslogin = "TRUE"
   }
+  scheduling {
+    preemptible = "true"
+    automatic_restart = "false"
+    }
 }
 
 output "instance_ip_addr" {
