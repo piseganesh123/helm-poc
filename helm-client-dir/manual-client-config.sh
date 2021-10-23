876e7b7513226c400c2e142bd918e94755918129
Ganesh Pise <piseganesh123@gmail.com>
2021-10-23 13:43:58 +0530

    auth-activate-account

--- helm-client-dir/manual-client-config.sh
+++ helm-client-dir/manual-client-config.sh
@@ -1,6 +1,5 @@
 #! /bin/bash
-gcloud auth login
+gcloud auth activate-service-account --key-file=/home/pgan432_gmail_com/gke-creator-level-epoch.json
 
 gcloud container clusters get-credentials helm-gke-cluster --zone asia-south1-c
 
-helm init --history-max 200
