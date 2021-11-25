ed86133fba15970b6f99485897c039ac7d02c24f
piseganesh123 <piseganesh123@gmail.com>
2021-11-25 18:35:25 +0530

    updated mysql update

--- helm-client-dir/manual-client-config.sh
+++ helm-client-dir/manual-client-config.sh
@@ -5,5 +5,15 @@ gcloud container clusters get-credentials helm-gke-cluster --zone asia-south1-c
 
 helm create mychart
 
+#get mysql pass
 
+
+#install mysql client
+kubectl run -i --tty ubuntu --image=ubuntu:16.04 --restart=Never -- bash -il
+
+#install tools inside container
+apt-get update && apt-get install mysql-client -y
+
+#to inspect helm chart
+helm inspect stable/mysql
 
